f79a2389a140aa5bee06f6a6d0f274ad1af2074e
YoussefAt928 <youssef@reyadabusiness.com>
2025-11-19 17:07:55 +0200

    Add test deployment workflow

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>رفع ملف CS2V</title>
+    <title>رفع ملف CS23V</title>
     <style>
         body {
             font-family: Arial, sans-serif;
